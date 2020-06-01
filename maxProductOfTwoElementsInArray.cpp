859c92636be4ea82703c914d220b0f961e1286db
pinkymangla <65814649+pinkymangla@users.noreply.github.com>
2020-06-01 19:46:25 +0530

    maxProductOfTwoElementsInarray

---
+++ maxProductOfTwoElementsInArray.cpp
@@ -0,0 +1,28 @@
+/*
+* Author : Pinky Mangla
+* Date : May 2020
+* Compiler : g++ 4.9.2
+* Flags : -std=c++14
+* Time complexity :O(n^2)
+* Title : maximum-product-of-two-elements-in-an-array
+*/
+
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int temp;
+        for(int i=0;i<nums.size();i++){
+            for(int j=i+1;j<nums.size();j++){
+                if(nums[i]<nums[j]){
+                    temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+            }
+                
+            }
+        }
+        
+        int x = (nums[0]-1)*(nums[1]-1);
+        return x;
+    }
+};
