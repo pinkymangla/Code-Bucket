10b5f583b371ba838861a15c2b2053ecbf4f826f
pinkymangla <65814649+pinkymangla@users.noreply.github.com>
2020-06-01 19:52:27 +0530

    findNumbersWithEvenNoOfDigits

---
+++ findNumbersWithEvenNoOfDigits.cpp
@@ -0,0 +1,21 @@
+/*
+* Author : Pinky Mangla
+* Date : May 2020
+* Compiler : g++ 4.9.2
+* Flags : -std=c++14
+* Time complexity :O(n)
+* Title : smaller number than current
+*/
+class Solution {
+public:
+    int findNumbers(vector<int>& nums) {
+       
+        int count = 0;
+        for(int i=0;i<nums.size();i++){
+           
+            if(to_string(nums[i]).length()%2==0)
+                count++;
+        }
+        return count;
+    }
+};
