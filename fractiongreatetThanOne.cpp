90a0e0d9334e1613a923a87d51f57ac48b88ce40
pinkymangla <65814649+pinkymangla@users.noreply.github.com>
2020-06-05 18:42:31 +0530

    fractionGreaterThanOne

---
+++ fractiongreatetThanOne.cpp
@@ -0,0 +1,25 @@
+/*
+* Author : Pinky Mangla
+* Date : May 2020
+* Compiler : g++ 4.9.2
+* Flags : -std=c++14
+* Time complexity :O(n)
+* Title : fraction greater than one
+* Example : greaterThanOne("1/2") -> false
+greaterThanOne("7/4") -> true
+*/
+bool greaterThanOne(std::string frac) {
+	char f1=frac[0];
+ char f2=frac[1];
+	int x=0;
+	int y=0;
+	std::stringstream s1(f1);
+		s1>>x;
+	std::stringstream s2(f1);
+		s2>>y;
+	int fraction = x/y;
+	if(fraction>1)
+		return true;
+	else 
+		return false;
+}
