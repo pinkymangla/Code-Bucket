85ffd544f0475169bf0b7912cc9deba7d3133dec
pinkymangla <65814649+pinkymangla@users.noreply.github.com>
2020-07-26 14:47:16 +0530

    check two strings

---
+++ string/check_if_string_can_be_obtained_by_rotating_another_string_2_places.cpp
@@ -0,0 +1,27 @@
+/*
+* Author : Pinky Mangla
+* Date : july 2020
+* Compiler : g++ 4.9.2
+* Flags : -std=c++14
+* Time complexity :O(n)
+* Title : check if string can be obtained by rotating another string two places 
+*/
+#include<bits/stdc++.h>
+using namespace std;
+bool isRotated(string& str1,string& str2){
+	if(str1.length()!=str2.length()){
+		return false;
+	}
+	string clock_rot="";
+	string anticlock_rot = "";
+	int len = str2.length();
+	anticlock_rot += str2.substr(len-2,2)+ str2.substr(0,len-2);
+	clock_rot += str2.substr(2)+str2.substr(0,2);
+	return(str1.compare(clock_rot)==0|| str1.compare(anticlock_rot)==0);
+}
+int main(){
+	string str1 = "geeks";
+	string str2 = "eksge";
+	isRotated(str1,str2)?cout<<"yes": cout<<"no";
+	return 0;
+}
